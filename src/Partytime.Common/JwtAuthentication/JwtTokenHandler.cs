f5f064a6c4afcfd13d294d4e3907a818cdca9e08
Tommie Olislagers <i457616@fhict.nl>
2023-06-13 19:54:42 +0200

    Removed files that were not needed and fixed code for user authentication

--- src/Partytime.Common/JwtAuthentication/JwtTokenHandler.cs
+++ src/Partytime.Common/JwtAuthentication/JwtTokenHandler.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using Npgsql;
 using Partytime.Common.JwtAuthentication.Models;
 using RabbitMQ.Client;
 using System.IdentityModel.Tokens.Jwt;
@@ -13,33 +18,45 @@ namespace Partytime.Common.JwtAuthentication
     public class JwtTokenHandler
     {
         public const string JWT_SECURITY_KEY = "4931575L5X7lkupX61Zj9hWm2kV2bJ63hamt729p";
+        public const string UserDatabaseConnectionString = "Server=authentication.postgres.database.azure.com;Database=users;Port=5432;User Id=tomoli;Password=34Bn473OaN76qBT7;Ssl Mode=Require;Trust Server Certificate=true;";
         private const int JWT_TOKEN_VALIDITY_MINS = 20;
-        private readonly List<UserAccount> _userAccountList;
 
-        public JwtTokenHandler()
+
+        public async Task<UserAccount?> GetUser(string username, string password)
         {
-        }
+            UserAccount account = new UserAccount();
+            NpgsqlConnection conn = new NpgsqlConnection(UserDatabaseConnectionString);
 
-        public async Task<string> UserAuthentication(string username, string password)
-        {
-            var pairs = new List<KeyValuePair<string, string>>
+            conn.Open();
+
+            // Obtain user
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM users WHERE username = '" + username + "'", conn);
+
+            // Execute the query and obtain a result set
+            NpgsqlDataReader reader = await command.ExecuteReaderAsync();
+
+            while (reader.Read())
             {
-                new KeyValuePair<string, string>("username", username),
-                new KeyValuePair<string, string>("password", password),
-            };
-
-            var content = new FormUrlEncodedContent(pairs);
-
-            var client = new HttpClient { BaseAddress = new Uri("http://localhost:8001") };
-
-            var response = await client.PostAsync("/user", content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadAsStringAsync();
+                account.Id = new Guid(reader["id"].ToString() ?? "");
+                account.Username = reader["username"].ToString() ?? "";
+                account.Email = reader["email"].ToString() ?? "";
+                account.Password = reader["password"].ToString() ?? "";
+                account.Role = reader["role"].ToString() ?? "";
             }
-                
-            return "";
+
+            reader.Close();
+
+            command.Dispose();
+            conn.Close();
+
+            bool isValidPassword = BCrypt.Net.BCrypt.Verify(password, account.Password);
+
+            if (isValidPassword)
+            {
+                return account;
+            }
+
+            return null;
         }
 
         public async Task<AuthenticationResponse?> GenerateJwtToken(AuthenticationRequest authenticationRequest)
@@ -47,19 +64,15 @@ namespace Partytime.Common.JwtAuthentication
             if (string.IsNullOrWhiteSpace(authenticationRequest.Username) || string.IsNullOrWhiteSpace(authenticationRequest.Password))
                 return null;
 
-            // Send client request to gateway for user authentication
-            string isUserInDatabase = await UserAuthentication(authenticationRequest.Username, authenticationRequest.Password);
-            if (isUserInDatabase.Length == 0) return null;
-
             // Convert client data from json string to useraccount object
-            var useraccount = JsonConvert.DeserializeObject<UserAccount>(isUserInDatabase);
+            UserAccount? useraccount = await GetUser(authenticationRequest.Username, authenticationRequest.Password);
             if(useraccount == null) return null;
 
             var tokenExpiryTimeStamp = DateTime.Now.AddMinutes(JWT_TOKEN_VALIDITY_MINS);
             var tokenKey = Encoding.ASCII.GetBytes(JWT_SECURITY_KEY);
             var claimsIdentity = new ClaimsIdentity(new List<Claim>
             {
-                new Claim(ClaimTypes.Name, useraccount.Userid.ToString()),
+                new Claim(ClaimTypes.Name, useraccount.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.Email, useraccount.Email),
                 new Claim(JwtRegisteredClaimNames.Name, authenticationRequest.Username),
                 new Claim("Role", useraccount.Role),
@@ -82,7 +95,7 @@ namespace Partytime.Common.JwtAuthentication
 
             return new AuthenticationResponse
             {
-                Userid = useraccount.Userid,
+                Userid = useraccount.Id,
                 Email = useraccount.Email,
                 Username = useraccount.Username,
                 Role = useraccount.Role,
--- src/Partytime.Common/JwtAuthentication/Models/UserAccount.cs
+++ src/Partytime.Common/JwtAuthentication/Models/UserAccount.cs
@@ -2,7 +2,7 @@
 {
     public class UserAccount
     {
-        public Guid Userid { get; set; }
+        public Guid Id { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
